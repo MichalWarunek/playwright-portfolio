a8ca116034fc5d30cc9c186db1ade5641d8a3168
Michał Warunek <michal.warunek@gmail.com>
2026-05-07 16:17:48 +0200

    test(cart): implement parameterized product actions and badge validations

--- pages/ProductsPage.ts
+++ pages/ProductsPage.ts
@@ -12,25 +12,44 @@ export class ProductsPage {
   private readonly page: Page;
   readonly productLocator: Locator;
   readonly removeLocator: Locator;
+  readonly secondProductLocator: Locator;
+  readonly removeSecondProductLocator: Locator;
+  readonly cart: Locator;
+  readonly cartBadge: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.productLocator = page.getByTestId("add-to-cart-sauce-labs-backpack");
     this.removeLocator = page.getByTestId("remove-sauce-labs-backpack");
+    this.secondProductLocator = page.getByTestId("add-to-cart-sauce-labs-bike-light");
+    this.removeSecondProductLocator = page.getByTestId("remove-sauce-labs-bike-light");
+    this.cart = page.getByTestId("shopping-cart-link");
+    this.cartBadge = page.getByTestId("shopping-cart-badge");
   }
 
 
-  async addToCart(): Promise<void> {
-    await this.productLocator.click();
+  async addToCart(product: Locator): Promise<void> {
+    await product.click();
     
   }
-  async removeFromCart(): Promise<void> {
-    await this.removeLocator.click();
+  async removeFromCart(product: Locator): Promise<void> {
+    await product.click();
   }
-  async validateAddButtonText(): Promise<void> {
-    await expect(this.productLocator).toHaveText(CART_BUTTON_TEXT.add);
+
+  
+  async validateAddButtonText(product: Locator): Promise<void> {
+    await expect(product).toHaveText(CART_BUTTON_TEXT.add);
+    
 }
-async validateRemoveButtonText(): Promise<void> {
-  await expect(this.removeLocator).toHaveText(CART_BUTTON_TEXT.remove);
+async validateRemoveButtonText(product: Locator): Promise<void> {
+  await expect(product).toHaveText(CART_BUTTON_TEXT.remove);
+  
+}
+async validateAddToCart(numberOfProducts: string): Promise<void> {
+  await expect(this.cartBadge).toBeVisible();
+  await expect(this.cartBadge).toHaveText(numberOfProducts);
+}
+async validateRemoveFromCart(): Promise<void> {
+  await expect(this.cartBadge).toBeHidden();
 }
 }
--- tests/addToCart.spec.ts
+++ tests/addToCart.spec.ts
@@ -14,14 +14,28 @@ test.describe("Add to cart test", () => {
   });
 
   test("should add product to cart", async ({productsPage}) => {
-    await productsPage.addToCart();
-    await productsPage.validateRemoveButtonText();
+    await productsPage.addToCart(productsPage.productLocator);
+    await productsPage.validateRemoveButtonText(productsPage.removeLocator);
+    await productsPage.validateAddToCart('1');
   });
   
   test("should remove product from cart", async ({productsPage}) => {
-    await productsPage.addToCart();
-    await productsPage.validateRemoveButtonText();
-    await productsPage.removeFromCart();
-    await productsPage.validateAddButtonText();
+    await productsPage.addToCart(productsPage.productLocator);
+    await productsPage.validateRemoveButtonText(productsPage.removeLocator);
+    await productsPage.removeFromCart(productsPage.removeLocator);
+    await productsPage.validateAddButtonText(productsPage.productLocator);
+    await productsPage.validateRemoveFromCart();
+  });
+
+  test("should add two products to cart and remove one", async ({productsPage}) => {
+    await productsPage.addToCart(productsPage.productLocator);
+    await productsPage.validateRemoveButtonText(productsPage.removeLocator);
+    await productsPage.validateAddToCart('1');
+    await productsPage.addToCart(productsPage.secondProductLocator);
+    await productsPage.validateRemoveButtonText(productsPage.removeSecondProductLocator);
+    await productsPage.validateAddToCart('2');
+    await productsPage.removeFromCart(productsPage.removeLocator);
+    await productsPage.validateAddButtonText(productsPage.productLocator);
+    await productsPage.validateAddToCart('1');
   });
 });
