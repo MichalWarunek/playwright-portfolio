c2b259e84e639aeb3d24fad96daa98a9547803e1
Michał Warunek <michal.warunek@gmail.com>
2026-05-13 17:31:06 +0200

    test(cart): add cart page validations

--- pages/CartPage.ts
+++ pages/CartPage.ts
@@ -1,24 +1,33 @@
 import { expect, Page, Locator } from "@playwright/test";
 
-export interface CartPageInfo {
-    title?: string;
-}
-
-export const cartData = {
-    title: 'Your Cart'
-} as const;
 
 export class CartPage {
   
   private readonly page: Page;
   readonly titleLabel: Locator;
+  readonly itemsCounter: Locator;
+  readonly inventoryName: Locator;
+  readonly inventoryPrice: Locator;
+  private readonly EXPECTED_TITLE = 'Your Cart';
 
   constructor(page: Page) {
     this.page = page;
     this.titleLabel = page.getByTestId('title');
+    this.itemsCounter = page.getByTestId('item-quantity');
+    this.inventoryName = page.getByTestId('inventory-item-name');
+    this.inventoryPrice = page.getByTestId('inventory-item-price');
   }
-  async validate(data: CartPageInfo = cartData): Promise<void> {
-     await expect(this.titleLabel).toHaveText(cartData.title);
 
+async validate(): Promise<void> {
+  await expect(this.titleLabel).toHaveText(this.EXPECTED_TITLE);
+}
+async validateItems (itemsQuantity: string): Promise<void> {
+  await expect(this.itemsCounter).toHaveText(itemsQuantity);
+}
+async validateInventoryName (itemNames: string): Promise<void> {
+  await expect(this.inventoryName).toHaveText(itemNames);
+}
+async validatePrice (price: string): Promise<void> {
+  await expect(this.inventoryPrice).toHaveText(price);
 }
 }
--- tests/addToCart.spec.ts
+++ tests/addToCart.spec.ts
@@ -3,9 +3,13 @@ import { test } from "../fixtures";
 const validLogin = {
   username: process.env.USER_EMAIL!,
   password: process.env.USER_PASSWORD!,
-  title: "Products",
 };
 
+const product = {
+  names: ['Sauce Labs Backpack'],
+  price: '$29.99',
+  quantity: '1'
+};
 
 test.describe("Add to cart test", () => {
   test.beforeEach(async ({ page, loginPage }) => {
@@ -16,14 +20,18 @@ test.describe("Add to cart test", () => {
   test("should add product to cart", async ({productsPage, cartPage}) => {
     await productsPage.addToCart(productsPage.productLocator);
     await productsPage.validateRemoveButtonText(productsPage.removeLocator);
-    await productsPage.validateAddToCart('1');
+    await productsPage.validateAddToCart(product.quantity);
     await productsPage.goToCart();
     await cartPage.validate();
+    await cartPage.validateItems(product.quantity);
+    await cartPage.validateInventoryName(product.names[0]);
+    await cartPage.validatePrice(product.price);
   });
   
   test("should remove product from cart", async ({productsPage}) => {
     await productsPage.addToCart(productsPage.productLocator);
     await productsPage.validateRemoveButtonText(productsPage.removeLocator);
+    await productsPage.validateAddToCart('1');
     await productsPage.removeFromCart(productsPage.removeLocator);
     await productsPage.validateAddButtonText(productsPage.productLocator);
     await productsPage.validateRemoveFromCart();
